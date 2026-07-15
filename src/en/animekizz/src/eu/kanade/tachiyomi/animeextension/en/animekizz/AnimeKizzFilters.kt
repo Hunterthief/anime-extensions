0ec3a508950625e9d79dd60f2d5ee6ca6c1e0ab9
Hunterthief <157222097+Hunterthief@users.noreply.github.com>
2026-07-15 15:38:38 +0000

    Refactor GenreFilter and add UriPartFilter

--- src/en/animekizz/src/eu/kanade/tachiyomi/animeextension/en/animekizz/AnimeKizzFilters.kt
+++ src/en/animekizz/src/eu/kanade/tachiyomi/animeextension/en/animekizz/AnimeKizzFilters.kt
@@ -3,34 +3,27 @@ package eu.kanade.tachiyomi.animeextension.en.animekizz
 import eu.kanade.tachiyomi.animesource.model.AnimeFilter
 import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
 
-abstract class SelectFilter(
-    displayName: String,
-    private val options: Array<Pair<String, String>>,
-) : AnimeFilter.Select(
-    displayName,
-    options.map { it.first }.toTypedArray(),
-) {
-    val selectedValue: String
-        get() = options[state].second
-}
-
-class GenreFilter : SelectFilter(
-    "Genre",
-    arrayOf(
-        Pair("All", ""),
-        Pair("Action", "Action"),
-        Pair("Adventure", "Adventure"),
-        Pair("Comedy", "Comedy"),
-        Pair("Drama", "Drama"),
-        Pair("Fantasy", "Fantasy"),
-        Pair("Supernatural", "Supernatural"),
-    )
-)
-
 object AnimeKizzFilters {
-    fun getFilterList(): AnimeFilterList {
-        return AnimeFilterList(
-            GenreFilter(),
+    fun getFilterList(): AnimeFilterList = AnimeFilterList(
+        AnimeFilter.Header("Text search ignores filters"),
+        GenreFilter()
+    )
+
+    private class GenreFilter : UriPartFilter(
+        "Genre",
+        arrayOf(
+            Pair("All", ""),
+            Pair("Action", "Action"),
+            Pair("Adventure", "Adventure"),
+            Pair("Comedy", "Comedy"),
+            Pair("Drama", "Drama"),
+            Pair("Fantasy", "Fantasy"),
+            Pair("Supernatural", "Supernatural"),
         )
+    )
+
+    private open class UriPartFilter(displayName: String, val vals: Array<Pair<String, String>>) : 
+        AnimeFilter.Select(displayName, vals.map { it.first }.toTypedArray()) {
+        fun toUriPart() = vals[state].second
     }
 }
