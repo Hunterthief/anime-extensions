6f9f8de14f190bea401c9835f9e2a2517cbe7c14
Hunterthief <157222097+Hunterthief@users.noreply.github.com>
2026-07-23 19:07:39 +0000

    Add MasterFilters for anime extension
    Implement master filters for anime extensions including genre, format, and sorting options.

---
+++ src/en/masterextension/src/eu/kanade/tachiyomi/animeextension/en/masterextension/MasterFilters.kt
@@ -0,0 +1,34 @@
+package eu.kanade.tachiyomi.animeextension.en.master
+
+import eu.kanade.tachiyomi.animesource.model.AnimeFilter
+import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
+
+object MasterFilters {
+
+    class GenreFilter : AnimeFilter.Select<String>(
+        "Genre",
+        arrayOf(
+            "Any", "Action", "Adventure", "Comedy", "Drama", "Fantasy", 
+            "Horror", "Mystery", "Romance", "Sci-Fi", "Slice of Life", 
+            "Sports", "Supernatural", "Thriller"
+        )
+    )
+
+    class FormatFilter : AnimeFilter.Select<String>(
+        "Format",
+        arrayOf("Any", "TV", "MOVIE", "OVA", "ONA", "SPECIAL")
+    )
+
+    class SortFilter : AnimeFilter.Select<String>(
+        "Sort By",
+        arrayOf("Popularity", "Average Score", "Newest", "Trending")
+    )
+
+    val filterList: AnimeFilterList = AnimeFilterList(
+        listOf(
+            GenreFilter(),
+            FormatFilter(),
+            SortFilter()
+        )
+    )
+}
