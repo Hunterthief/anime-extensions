6987838998c715b7d4223caf12e35741bd1b4c42
Hunterthief <157222097+Hunterthief@users.noreply.github.com>
2026-07-25 00:20:36 +0000

    Update ProviderManager.kt

--- src/en/masterextension/src/eu/kanade/tachiyomi/animeextension/en/masterextension/ProviderManager.kt
+++ src/en/masterextension/src/eu/kanade/tachiyomi/animeextension/en/masterextension/ProviderManager.kt
@@ -114,42 +114,6 @@ class ProviderManager(
         return Triple("", "S0", err.take(60))
     }
 
-    fun fetchAllAnimeEpisodes(showId: String): Triple<Map<String, String>, String, String> {
-        val query = """
-            query(${'$'}_id: String!) {
-              show(_id: ${'$'}_id) {
-                _id
-                episodes(translationType: "sub") {
-                  episodeString
-                  note
-                }
-              }
-            }
-        """.trimIndent()
-        
-        val payload = buildJsonObject {
-            put("query", query)
-            put("variables", buildJsonObject {
-                put("_id", showId)
-            })
-        }
-        
-        val res = makeAllAnimePostRequest(payload)
-        if (res != null && !res.startsWith("ERR") && !res.startsWith("EXC")) {
-            val eps = res.parseAs<AllAnimeResponse>().data?.show?.episodes
-            if (!eps.isNullOrEmpty()) {
-                val map = eps.mapNotNull { ep ->
-                    val title = ep.note?.takeIf { n -> n.isNotBlank() }
-                    if (title != null) ep.episodeString to title else null
-                }.toMap()
-                return Triple(map, "E1", "")
-            }
-        }
-
-        val err = res ?: "Null"
-        return Triple(emptyMap(), "E0", err.take(60))
-    }
-
     // --- MYANIMELIST HTML SCRAPER ---
 
     fun fetchMalEpisodes(malId: Int): Triple<Map<String, String>, String, String> {
