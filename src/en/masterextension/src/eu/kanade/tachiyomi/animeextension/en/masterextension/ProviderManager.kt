1ccf851601ad2ef0fee6aeba4f1f954ccfcc713f
Hunterthief <157222097+Hunterthief@users.noreply.github.com>
2026-07-26 00:46:02 +0000

    Update ProviderManager.kt

--- src/en/masterextension/src/eu/kanade/tachiyomi/animeextension/en/masterextension/ProviderManager.kt
+++ src/en/masterextension/src/eu/kanade/tachiyomi/animeextension/en/masterextension/ProviderManager.kt
@@ -5,6 +5,7 @@ import eu.kanade.tachiyomi.animesource.model.SAnime
 import eu.kanade.tachiyomi.animesource.model.SEpisode
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.animeextension.en.masterextension.videosources.AnimeKizzProvider
+import eu.kanade.tachiyomi.animeextension.en.masterextension.videosources.AniDapProvider
 import keiyoushi.utils.parallelCatchingFlatMap
 import okhttp3.Headers
 import okhttp3.OkHttpClient
@@ -24,7 +25,7 @@ class ProviderManager(
     // =================================================================
     private val allProviders: Map<String, VideoProvider> by lazy {
         linkedMapOf(
-            
+            "anidap"     to AniDapProvider(client, headers),
             "animekizz"  to AnimeKizzProvider(client, headers),
             // "example" to ExampleProvider(client, headers),  ← future sources
         )
