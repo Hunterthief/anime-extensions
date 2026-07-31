31d935f5d541997d15290dbd08729e7c159cded4
Hunterthief <157222097+Hunterthief@users.noreply.github.com>
2026-07-31 15:09:44 +0000

    Refactor ReAnime data classes for Flix response

--- src/en/masterextension/src/eu/kanade/tachiyomi/animeextension/en/masterextension/videosources/reanime/ReAnimeDtos.kt
+++ src/en/masterextension/src/eu/kanade/tachiyomi/animeextension/en/masterextension/videosources/reanime/ReAnimeDtos.kt
@@ -4,32 +4,13 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ReAnimeSearchResponse(
-    val results: List<ReAnimeSearchResult> = emptyList(),
-    val total: Int = 0,
+data class ReAnimeFlixResponse(
+    val success: Boolean = false,
+    val servers: List<ReAnimeFlixServer> = emptyList(),
 )
 
 @Serializable
-data class ReAnimeSearchResult(
-    @SerialName("anime_id") val animeId: String = "",
-    val title: ReAnimeTitle = ReAnimeTitle(),
-)
-
-@Serializable
-data class ReAnimeTitle(
-    val english: String = "",
-    val romaji: String = "",
-)
-
-@Serializable
-data class ReAnimeWatchResponse(
-    @SerialName("episode_links") val episodeLinks: List<ReAnimeServerLink> = emptyList(),
-    val current: Int = 0,
-    val duration: Int = 0,
-)
-
-@Serializable
-data class ReAnimeServerLink(
+data class ReAnimeFlixServer(
     @SerialName("serverName") val serverName: String = "",
     @SerialName("dataLink") val dataLink: String = "",
     @SerialName("dataType") val dataType: String = "",
