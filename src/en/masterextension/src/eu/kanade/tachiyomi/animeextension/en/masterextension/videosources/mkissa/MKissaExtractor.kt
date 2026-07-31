bf90242ae562df5e5d41b4b43ec08157419444ea
Hunterthief <157222097+Hunterthief@users.noreply.github.com>
2026-07-31 02:55:06 +0000

    Update MKissaExtractor.kt

--- src/en/masterextension/src/eu/kanade/tachiyomi/animeextension/en/masterextension/videosources/mkissa/MKissaExtractor.kt
+++ src/en/masterextension/src/eu/kanade/tachiyomi/animeextension/en/masterextension/videosources/mkissa/MKissaExtractor.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.animeextension.en.masterextension.mkissa
+package eu.kanade.tachiyomi.animeextension.en.masterextension.videosources.mkissa
 
 import aniyomi.lib.playlistutils.PlaylistUtils
 import eu.kanade.tachiyomi.animesource.model.Track
