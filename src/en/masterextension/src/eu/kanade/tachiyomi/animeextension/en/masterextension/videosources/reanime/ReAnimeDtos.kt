b10ece74f1b8951f2daa47cff667acc2165b68cc
Hunterthief <157222097+Hunterthief@users.noreply.github.com>
2026-07-31 14:58:18 +0000

    Update ReAnimeDtos.kt

--- src/en/masterextension/src/eu/kanade/tachiyomi/animeextension/en/masterextension/videosources/reanime/ReAnimeDtos.kt
+++ src/en/masterextension/src/eu/kanade/tachiyomi/animeextension/en/masterextension/videosources/reanime/ReAnimeDtos.kt
@@ -13,26 +13,24 @@ data class ReAnimeSearchResponse(
 data class ReAnimeSearchResult(
     @SerialName("anime_id") val animeId: String = "",
     val title: ReAnimeTitle = ReAnimeTitle(),
-    @SerialName("cover_image") val coverImage: ReAnimeCoverImage = ReAnimeCoverImage(),
-    val format: String = "",
-    val episodes: Int = 0,
-    val subbed: Int = 0,
-    val dubbed: Int = 0,
-    @SerialName("average_score") val averageScore: Int = 0,
-    val genres: List<String> = emptyList(),
-    @SerialName("season_year") val seasonYear: Int = 0,
 )
 
 @Serializable
 data class ReAnimeTitle(
     val english: String = "",
-    val native: String = "",
     val romaji: String = "",
 )
 
 @Serializable
-data class ReAnimeCoverImage(
-    @SerialName("extra_large") val extraLarge: String = "",
-    val large: String = "",
-    val medium: String = "",
+data class ReAnimeWatchResponse(
+    @SerialName("episode_links") val episodeLinks: List<ReAnimeServerLink> = emptyList(),
+    val current: Int = 0,
+    val duration: Int = 0,
+)
+
+@Serializable
+data class ReAnimeServerLink(
+    @SerialName("serverName") val serverName: String = "",
+    @SerialName("dataLink") val dataLink: String = "",
+    @SerialName("dataType") val dataType: String = "",
 )
