e34ee66d02bb5980c3a8fcf56a6870e2b09fecf9
Hunterthief <157222097+Hunterthief@users.noreply.github.com>
2026-08-01 14:59:50 +0000

    Update ReAnimeDtos.kt

--- src/en/masterextension/src/eu/kanade/tachiyomi/animeextension/en/masterextension/videosources/reanime/ReAnimeDtos.kt
+++ src/en/masterextension/src/eu/kanade/tachiyomi/animeextension/en/masterextension/videosources/reanime/ReAnimeDtos.kt
@@ -4,6 +4,24 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
+data class ReAnimeSearchResponse(
+    val results: List<ReAnimeSearchResult> = emptyList(),
+    val total: Int = 0,
+)
+
+@Serializable
+data class ReAnimeSearchResult(
+    @SerialName("anime_id") val animeId: String = "",
+    val title: ReAnimeTitle = ReAnimeTitle(),
+)
+
+@Serializable
+data class ReAnimeTitle(
+    val english: String = "",
+    val romaji: String = "",
+)
+
+@Serializable
 data class ReAnimeFlixResponse(
     val success: Boolean = false,
     val servers: List<ReAnimeFlixServer> = emptyList(),
