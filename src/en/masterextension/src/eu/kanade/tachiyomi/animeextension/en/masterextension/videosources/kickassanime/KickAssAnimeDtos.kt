4f24db65349f0e2b56b81d1a3bd61a7bdbb2087d
Hunterthief <157222097+Hunterthief@users.noreply.github.com>
2026-08-01 15:58:14 +0000

    Update KickAssAnimeDtos.kt

--- src/en/masterextension/src/eu/kanade/tachiyomi/animeextension/en/masterextension/videosources/kickassanime/KickAssAnimeDtos.kt
+++ src/en/masterextension/src/eu/kanade/tachiyomi/animeextension/en/masterextension/videosources/kickassanime/KickAssAnimeDtos.kt
@@ -2,55 +2,41 @@ package eu.kanade.tachiyomi.animeextension.en.masterextension.videosources.kicka
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.JsonObject
 
 @Serializable
-data class SearchResponseDto(
-    val result: List<PopularItemDto>,
-    val maxPage: Int,
+data class KickAssAnimeSearchResponse(
+    val result: List<KickAssAnimeSearchResult> = emptyList(),
+    val maxPage: Int = 0,
 )
 
 @Serializable
-data class PopularItemDto(
+data class KickAssAnimeSearchResult(
     val title: String,
-    val title_en: String?,
-    val slug: String,
-    val poster: PosterDto,
+    val poster: String,
+    val id: Int,
 )
 
 @Serializable
-data class PosterDto(@SerialName("hq") val slug: String) {
-    val url by lazy { "image/poster/$slug.webp" }
-}
+data class KickAssAnimeEpisodeResponse(
+    @SerialName("current_page") val currentPage: Int,
+    @SerialName("last_page") val lastPage: Int,
+    val data: List<KickAssAnimeEpisode> = emptyList(),
+)
 
 @Serializable
-data class EpisodeResponseDto(
-    val pages: List<JsonObject>,
-    val result: List<EpisodeDto> = emptyList(),
-) {
-    @Serializable
-    data class EpisodeDto(
-        val slug: String,
-        val title: String?,
-        val episode_string: String,
-    )
-}
+data class KickAssAnimeEpisode(
+    @SerialName("created_at") val createdAt: String,
+    val session: String,
+    @SerialName("episode") val episodeNumber: Float,
+)
 
 @Serializable
-data class ServersDto(val servers: List<Server>) {
-    @Serializable
-    data class Server(
-        val name: String,
-        val src: String,
-    )
-}
+data class KickAssAnimeServerResponse(
+    val servers: List<KickAssAnimeServer> = emptyList(),
+)
 
 @Serializable
-data class VideoDto(
-    val hls: String = "",
-    val dash: String = "",
-    val subtitles: List<SubtitlesDto> = emptyList(),
-) {
-    @Serializable
-    data class SubtitlesDto(val name: String, val language: String, val src: String)
-}
+data class KickAssAnimeServer(
+    val name: String,
+    val src: String,
+)
